a18926b5333131698bd1bb0de3837601a4b8f400
Nadine <nadlgit@users.noreply.github.com>
2022-06-01 23:15:39 -0400

    feat: header component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,3 +1,10 @@
+import styles from './app.module.css';
 import { ChallengeAttribution } from 'components/challenge-attribution';
+import { Header } from 'components/header';
 
-export const App = () => <ChallengeAttribution />;
+export const App = () => (
+  <div className={styles.app}>
+    <Header score={12} />
+    <ChallengeAttribution />
+  </div>
+);
---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import styles from './header.module.css';
+import Logo from './logo.svg';
+
+export const Header = ({ score = 0 }) => (
+  <header className={styles.header}>
+    <img className={styles.logo} src={Logo} alt="Rock, Paper, Scissors logo" />
+    <section className={styles.score}>
+      <h3>Score</h3>
+      <p>{score}</p>
+    </section>
+  </header>
+);
