b0875db5dfb658da27f31a30177978ce2d63aead
Nadine <nadlgit@users.noreply.github.com>
2022-06-02 15:34:28 -0400

    feat: footer component

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,10 +1,12 @@
 import styles from './app.module.css';
 import { ChallengeAttribution } from 'components/challenge-attribution';
 import { Header } from 'components/header';
+import { Footer } from 'components/footer';
 
 export const App = () => (
   <div className={styles.app}>
     <Header score={12} />
+    <Footer />
     <ChallengeAttribution />
   </div>
 );
