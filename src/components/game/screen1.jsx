1381be8fcd42bac4962d97a1727fc1c5d414acf5
Nadine <nadlgit@users.noreply.github.com>
2022-06-03 19:25:14 -0400

    feat: CSS: positioned hands in triangle

--- src/components/game/screen1.jsx
+++ src/components/game/screen1.jsx
@@ -5,13 +5,13 @@ import { Item } from './item';
 export const Screen1 = () => {
   return (
     <div className={styles.screen}>
-      <button>
+      <button className={styles.paper}>
         <Item type={ITEM_TYPES.PAPER} />
       </button>
-      <button>
+      <button className={styles.scissors}>
         <Item type={ITEM_TYPES.SCISSORS} />
       </button>
-      <button>
+      <button className={styles.rock}>
         <Item type={ITEM_TYPES.ROCK} />
       </button>
     </div>
