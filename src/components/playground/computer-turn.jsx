2f6d4679871a9fd479f5d54af2a6acea72be2b8a
Nadine <nadlgit@users.noreply.github.com>
2022-06-05 21:38:06 -0400

    feat: css updates (results display)

--- src/components/playground/computer-turn.jsx
+++ src/components/playground/computer-turn.jsx
@@ -1,16 +1,28 @@
 import styles from './computer-turn.module.css';
 import { Item } from './item';
 
+// const tmpSleep = (ms) => {
+//   const date = Date.now();
+//   let currentDate = null;
+//   do {
+//     currentDate = Date.now();
+//   } while (currentDate - date < ms);
+// };
+
 export const ComputerTurn = ({
   playerChoice = '',
   computerChoice = '',
   result = '',
   onRestart = () => {},
 }) => {
+  // if (computerChoice) tmpSleep(500);
+  // if (result) tmpSleep(500);
   return (
     <div className={styles.container}>
       <p className={styles.text1}>You picked</p>
-      <Item className={styles.item1} type={playerChoice} />
+      <div className={`${styles.item1} ${result && styles.highlight}`}>
+        <Item type={playerChoice} />
+      </div>
       <p className={styles.text2}>The House picked</p>
       <Item className={styles.item2} type={computerChoice} />
       <div className={styles.result}>
